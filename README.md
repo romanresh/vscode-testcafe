81dde42a17f3522f06ddb1682734b441976e8de3
Roman Reshetnikov <roman.reshetnikov@devexpress.com>
2016-12-15 15:31:58 +0300

    Repeat Test Run (#5), refactoring, Fix error while extension is not activated but menu items are visible.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # TestCafe Runner for Visual Studio Code
+This extension allows to run TestCafe tests directly from VS Code via context menu or built-in commands:
+* TestCafe: Run Test (Chrome)
+* TestCafe: Run Test (IE)
+* TestCafe: Run Test (Firefox)
+* TestCafe: Repeat Previous Test Run
 
 Current Limitations
 -----
@@ -7,3 +12,7 @@ Current Limitations
 2. VS Code Project folder should contains node_modules folder with TestCafe
 3. Tests are always run in the debug mode.
 4. Only .js test files are supported
+
+Source
+-----
+[GitHub](https://github.com/romanresh/vscode-testcafe)
--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,45 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let controller = new TestCafeTestController();
+    
+    vscode.commands.executeCommand('setContext', 'testcaferunner.canRerun', false);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "testcafe-test-runner" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-
-    let finder = new TestFinder();
     context.subscriptions.push(
         vscode.commands.registerCommand('testcaferunner.runIE', () => {
-            finder.runCurrentTest("ie");
+            controller.runCurrentTest("ie");
         })
     );
     context.subscriptions.push(
         vscode.commands.registerCommand('testcaferunner.runFirefox', () => {
-            finder.runCurrentTest("firefox");
+            controller.runCurrentTest("firefox");
         })
     );
     context.subscriptions.push(
         vscode.commands.registerCommand('testcaferunner.runChrome', () => {
-            finder.runCurrentTest("chrome");
+            controller.runCurrentTest("chrome");
         })
     );
-    context.subscriptions.push(finder);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('testcaferunner.repeatRun', () => {
+            controller.repeatLastTest();
+        })
+    );
+    context.subscriptions.push(controller);
+    vscode.commands.executeCommand('setContext', 'testcaferunner.readyForUX', true);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
 
-class TestFinder {
+class TestCafeTestController {
+    lastBrowser: string;
+    lastFile: string;
+    lastTest: string;
+
     public runCurrentTest(browser: string) {
         let editor = vscode.window.activeTextEditor;
         if(!editor)
@@ -47,7 +48,14 @@ class TestFinder {
         if (doc.languageId !== "javascript")
             return;
         var test = this.findTest(editor.document, editor.selection);
-        this.runTest(test, browser);
+        this.runTest(test, browser, editor.document.fileName);
+    }
+    public repeatLastTest() {
+        if(!this.lastTest || !this.lastBrowser || !this.lastFile) {
+             vscode.window.showErrorMessage(`Previous test is not found.`);
+             return;
+        }
+        this.runTest(this.lastTest, this.lastBrowser, this.lastFile);
     }
     private findTest(document: vscode.TextDocument, selection: vscode.Selection): string {
         var text = document.getText(new vscode.Range(0, 0, selection.end.line, selection.end.character));
@@ -66,11 +74,14 @@ class TestFinder {
         
         return tests[tests.length - 1];
     }
-    public runTest(name: string, browser: string) {
+    public runTest(name: string, browser: string, filePath: string) {
         if(!name) {
-             vscode.window.showErrorMessage(`TestCafe test is not found`);
+             vscode.window.showErrorMessage(`Test is not found: test() function is not found or it's commented. Set cursor inside test() function`);
              return;
         }
+        this.lastBrowser = browser;
+        this.lastTest = name;
+        this.lastFile = filePath;
         //vscode.window.showInformationMessage(`Running test: ${name}`);
         vscode.commands.executeCommand("vscode.startDebug", {
             "type": "node",
@@ -79,15 +90,16 @@ class TestFinder {
             "program": "${workspaceRoot}/node_modules/testcafe/bin/testcafe.js",
             "args": [
                 browser,
-                "${file}",
+                filePath,
                 "--test",
                 name.substr(1, name.length - 2)
             ],
             "cwd": "${workspaceRoot}"
         });
+        vscode.commands.executeCommand('setContext', 'testcaferunner.canRerun', true);
     }
 
-    dispose() {
+    dispose() { 
         
     }
 }
